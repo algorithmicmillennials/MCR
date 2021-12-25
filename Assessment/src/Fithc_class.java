7108b5ec17d3abed0e9e57b973a78928dc6488b9
algorithmicmillennials <95002375+algorithmicmillennials@users.noreply.github.com>
2021-12-25 14:42:41 -0800

    Update Fithc_class.java

--- Assessment/src/Fithc_class.java
+++ Assessment/src/Fithc_class.java
@@ -6,23 +6,31 @@ import java.util.Scanner;
 
 public class Fithc_class {
 	public static void main(String[] args) {
+		//enter program set two int variable to 0
 		int op = 0;
 		int ch=0;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("name two files.");
+		//take in two folder name
 		String First_file = scan.next();
 		String Second_file = scan.next();
+		//store the folders in Desktop Folder
 		File f = new File("C:\\Users\\Duy Dang\\Desktop\\"+First_file+"\\"+Second_file);
 		f.mkdirs();
+		//enter main loop
 		while(op==0) {
+			//set ch to 0
 			ch=0;
+			//print menu
 		System.out.println("Welcome Please Select From the Menu");
 		System.out.println("1. Retrieve all files inside main");
 		System.out.println("2. Display Secondary Menu for Performing File Operations and Take user input");
 		System.out.println("3. Program Termination");
+			//take in switch case
 		int choice = scan.nextInt();
 		switch(choice)
 		{
+				//choice one prints Folder directory
 		case 1:
 			//some code
 			File ob = new File("C:\\Users\\Duy Dang\\Desktop\\"+First_file+"\\");
@@ -37,7 +45,7 @@ public class Fithc_class {
 		case 2:
 			//some code
 			
-			
+			//enter second loop new menu
 			while (ch==0) {
 				System.out.println("Welcome Please Select From the Menu");
 				System.out.println("1.Add a Text file to main");
@@ -49,6 +57,7 @@ public class Fithc_class {
 				int Choice_2 = newscan.nextInt();
 			
 			if(Choice_2==1) {
+				//create text file
 				//some code
 				System.out.println("name your text file");
 				String Textfile_name = scan.next();
@@ -63,7 +72,7 @@ public class Fithc_class {
 			}
 				
 			else if(Choice_2 ==2) {
-				
+				//show file to delete
 				System.out.println("Available File or Folder for Deletion");
 				File delete_file = new File("C:\\Users\\Duy Dang\\Desktop\\"+First_file+"\\"+Second_file);
 				File array_file[] = delete_file.listFiles();
@@ -76,7 +85,7 @@ public class Fithc_class {
 				//some code
 			}
 			else if(Choice_2 == 3) {
-				
+				//call sortby_name class and its method
 				Sortby_name objsort = new Sortby_name();
 				objsort.sortarry(First_file, Second_file);
 		
@@ -87,20 +96,22 @@ public class Fithc_class {
 			
 			
 			else if (Choice_2 == 4) {
-				System.out.println("update ch in while loop");
-				
+				System.out.println();
+				//ch got updated exit loop go back to main loop
 				ch = 1;
 				//some code
 			}
 			else if (Choice_2 == 5) {
+				//ch and op are updated program ends
 				ch = 1;
 				op = 1;
+				System.out.println("Program has end");
 				//some code
 				
 				
 			}
 			else {
-			
+			        //wrong choice will take you back to last menu
 				System.out.println("Invalid choice");
 				
 							
